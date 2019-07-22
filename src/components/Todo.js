e02f3b208ab280e24b7585c5f3e9b3a65f71890e
Kira Yang <changru.yang02@gmail.com>
2019-07-22 23:08:23 +0800

    [feat]:todolist delete function

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,35 +1,89 @@
-import React from "react";
+import React, { Component } from "react";
+import TodoList from "./TodoList";
 import styles from "../styles/Todo.module.scss";
 
-const Todo = props => {
-  return (
-    <div
-      className={styles.todo}
-      style={{
-        transform: props.showTodo ? "translateX(0%)" : "translateX(-100%)"
-      }}
-    >
+class Todo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: "",
+      todoList: []
+    };
+  }
+  onChange = e => {
+    this.setState({
+      text: e.target.value
+    });
+    console.log(this.state.text);
+  };
+
+  onKeyDown = e => {
+    e.keyCode === 13 && this.addTodo();
+  };
+
+  addTodo = e => {
+    e.preventDefault();
+    let todolist = this.state.todoList;
+    let text = this.state.text;
+    let id = this.state.todoList.length + 1;
+    todolist.push({ text: text, completed: false, id: id });
+    this.setState({
+      todoList: todolist,
+      text: ""
+    });
+    console.log(e);
+  };
+
+  removeTodo = index => {
+    let todolist = this.state.todoList;
+    todolist.splice(index, 1);
+    this.setState({
+      todolist: todolist
+    });
+  };
+
+  render() {
+    return (
       <div
-        className={styles.container}
+        className={styles.todo}
         style={{
-          marginLeft: props.showTodo ? "160px" : "25px",
-          marginRight: props.showTodo ? "40px" : "25px"
+          transform: this.props.showTodo
+            ? "translateX(0%)"
+            : "translateX(-100%)"
         }}
       >
-        <div className={styles.panel}>
-          <h1>待辦清單</h1>
-          <div className={styles.status_panel}>
-            <button>未完成</button>
-            <button>已完成</button>
+        <div
+          className={styles.container}
+          style={{
+            marginLeft: this.props.showTodo ? "160px" : "25px",
+            marginRight: this.props.showTodo ? "40px" : "25px"
+          }}
+        >
+          <div className={styles.panel}>
+            <h1>待辦清單</h1>
+            <div className={styles.status_panel}>
+              <button>未完成</button>
+              <button>已完成</button>
+            </div>
           </div>
+          <form>
+            <input
+              type="text"
+              placeholder="新增待辦事項"
+              value={this.state.text}
+              onChange={this.onChange}
+              onKeyDown={e => this.onKeyDown}
+            />
+            <button onClick={this.addTodo}>+</button>
+          </form>
+          <TodoList
+            todolist={this.state.todoList}
+            removeTodo={this.removeTodo}
+          />
         </div>
-        <form>
-          <input type="text" placeholder="新增待辦事項" />
-          <button>+</button>
-        </form>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Todo;
---
+++ src/components/TodoList.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import styles from "../styles/TodoList.module.scss";
+import edit from "../images/icon-edit.svg";
+import remove from "../images/icon-cancel.svg";
+
+class TodoList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentFilter: "all"
+    };
+  }
+
+  render() {
+    const listItem = this.props.todolist.map((todo, index) => (
+      <li key={todo.id}>
+        <span>{todo.text}</span>
+        <div className={styles.buttons}>
+          <button>
+            <img src={edit} alt="edit" />
+          </button>
+          <button>
+            <img
+              src={remove}
+              alt="remove"
+              onClick={() => this.props.removeTodo(index)}
+            />
+          </button>
+        </div>
+      </li>
+    ));
+    return (
+      <div className={styles.todolist}>
+        <ul>{listItem}</ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
