9d9cca11ff68d46177fc229bd0f4360f54ef0c46
Kira Yang <changru.yang02@gmail.com>
2019-08-28 00:22:43 +0800

    deploy project

---
+++ build/precache-manifest.7503e7324372a1faa9f076148107ad83.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ef3d3d564f5a01cc8c0ff60170cc10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0a0dfba0cf97e934373",
+    "url": "/static/css/main.5cb494de.chunk.css"
+  },
+  {
+    "revision": "911db4b421b8f7239f2f",
+    "url": "/static/js/2.efdfb79e.chunk.js"
+  },
+  {
+    "revision": "a0a0dfba0cf97e934373",
+    "url": "/static/js/main.79c959ef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "526aed07ca647772b1c7e37fce521ba1",
+    "url": "/static/media/icon-bell.526aed07.svg"
+  },
+  {
+    "revision": "872654be6f7049208508b4b9dbb29ad9",
+    "url": "/static/media/icon-cancel.872654be.svg"
+  },
+  {
+    "revision": "0520b13cab358d27dd2e3bb97bf63090",
+    "url": "/static/media/icon-delete.0520b13c.svg"
+  },
+  {
+    "revision": "5094d45d055ee409d5375f38d8da5014",
+    "url": "/static/media/icon-pause--orange.5094d45d.svg"
+  },
+  {
+    "revision": "a185f0b008b9db222eb42d8126485a3d",
+    "url": "/static/media/icon-play--orange.a185f0b0.svg"
+  },
+  {
+    "revision": "4298a104d2031aa18070af31dbd4728f",
+    "url": "/static/media/tomato--green.4298a104.svg"
+  },
+  {
+    "revision": "f4d252253f2234eeed22a10c0daf96fb",
+    "url": "/static/media/tomato--orange.f4d25225.svg"
+  }
+]);
