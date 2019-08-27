e666523ae58ca58d7e518120d310fbad3f3761a8
Kira Yang <changru.yang02@gmail.com>
2019-08-28 00:32:16 +0800

    Updates

---
+++ precache-manifest.2e9430323769ba13fe10f3d4c48272a7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124afa0fb6e0a57f07df3bdcd170f816",
+    "url": "/Pomodoro/index.html"
+  },
+  {
+    "revision": "5b3f792e68a37797a044",
+    "url": "/Pomodoro/static/css/main.5cb494de.chunk.css"
+  },
+  {
+    "revision": "911db4b421b8f7239f2f",
+    "url": "/Pomodoro/static/js/2.efdfb79e.chunk.js"
+  },
+  {
+    "revision": "5b3f792e68a37797a044",
+    "url": "/Pomodoro/static/js/main.1e6880ff.chunk.js"
+  },
+  {
+    "revision": "80b30bda6765e0e31afe",
+    "url": "/Pomodoro/static/js/runtime~main.52a691fa.js"
+  },
+  {
+    "revision": "526aed07ca647772b1c7e37fce521ba1",
+    "url": "/Pomodoro/static/media/icon-bell.526aed07.svg"
+  },
+  {
+    "revision": "872654be6f7049208508b4b9dbb29ad9",
+    "url": "/Pomodoro/static/media/icon-cancel.872654be.svg"
+  },
+  {
+    "revision": "0520b13cab358d27dd2e3bb97bf63090",
+    "url": "/Pomodoro/static/media/icon-delete.0520b13c.svg"
+  },
+  {
+    "revision": "5094d45d055ee409d5375f38d8da5014",
+    "url": "/Pomodoro/static/media/icon-pause--orange.5094d45d.svg"
+  },
+  {
+    "revision": "a185f0b008b9db222eb42d8126485a3d",
+    "url": "/Pomodoro/static/media/icon-play--orange.a185f0b0.svg"
+  },
+  {
+    "revision": "4298a104d2031aa18070af31dbd4728f",
+    "url": "/Pomodoro/static/media/tomato--green.4298a104.svg"
+  },
+  {
+    "revision": "f4d252253f2234eeed22a10c0daf96fb",
+    "url": "/Pomodoro/static/media/tomato--orange.f4d25225.svg"
+  }
+]);
